f38f6330c0f7ce4ccf62434603f5a02b02457924
Michal Cierniak <cierniak@users.noreply.github.com>
2015-01-03 18:23:16 -0800

    First skeleton with static data in the client.

---
+++ client/templates/camps/camps_list.js
@@ -0,0 +1,14 @@
+var campsData = [
+    {
+        name: 'Coder Camps',
+        url: 'https://tcs-paloalto.frontdeskhq.com/courses/44546'
+    },
+    {
+        name: 'CSMA',
+        url: 'http://www.arts4all.org/study/camps.htm'
+    }
+];
+
+Template.campsList.helpers({
+    camps: campsData
+});
